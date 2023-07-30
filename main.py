f7a86a301b950777d2a16b14812aadc59d7b5f5d
vdsap <thevdsap@gmail.com>
2023-07-30 16:28:08 +0300

    added userbot, storage, exceprion to run

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from loguru import logger
 from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from datab import create_db
-from conf_init import conf_init
+from conf_init import conf_init, tgclient_init
 from commands.main_commands import commands
 from commands.admin.console_admin_commands import terminal_commands as term_commands
 from commands.admin.bot_inline_commands import inline_command as inline
@@ -17,23 +18,28 @@ async def on_startup(dp):
     logger.info('Bot started')
     await bot.send_message(821461129, "Bot started")
 
+
 logger.info('Init bot')
 conf = conf_init()
 bot = Bot(token=conf['api_token'])
 bot.parse_mode = "html"
+storage = MemoryStorage()
+tgclient = tgclient_init(conf)
 
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 logger.info('Init commands')
 commands(dp)
 term_commands(dp)
 inline(bot, dp)
 pub_commands(dp)
-admin_commands(dp)
+admin_commands(dp, bot, conf, tgclient)
 create_db()
 
 logger.info('Start bot')
 
 if __name__ == '__main__':
     logger.info('Polling started')
-    asyncio.run(executor.start_polling(dp, skip_updates=True, on_startup=on_startup))
-
+    try:
+        asyncio.run(executor.start_polling(dp, skip_updates=True, on_startup=on_startup))
+    except Exception as err:
+        logger.error(err)
