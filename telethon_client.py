fab56569b64461b513439e61b1f33e871076c600
vdsap <thevdsap@gmail.com>
2023-07-30 17:22:14 +0300

    userbot for samokat section

---
+++ telethon_client.py
@@ -0,0 +1,18 @@
+from asyncio import sleep
+from loguru import logger
+
+
+async def tgsendmes(client, text1, photo, text2):
+    async with client:
+        logger.debug('Sending texts')
+        for text in text1:
+            await client.send_message(5843562048, text)
+            await sleep(1)
+        logger.info('Texts sent')
+        await sleep(1)
+        logger.debug('Sending photo')
+        await client.send_file(5843562048, photo)
+        await sleep(1)
+        for txt in text2:
+            await client.send_message(5843562048, txt)
+            await sleep(1)
