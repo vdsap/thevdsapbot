a343963e6d73facc16153464fce229b41719472a
vdsap <thevdsap@gmail.com>
2023-07-30 16:29:49 +0300

    added logging into telethon

--- conf_init.py
+++ conf_init.py
@@ -1,5 +1,6 @@
 from yaml import safe_load as load
 from loguru import logger
+from telethon import TelegramClient
 
 def conf_init():
     with open('conf.yaml') as f:
@@ -15,3 +16,9 @@ def check_superadmin(user_id: int):
     else:
         logger.debug('{} is not superadmin'.format(user_id))
         return False
+
+def tgclient_init(config):
+    logger.debug('Signing in user')
+    client = TelegramClient(config['client_name'], config['api_id'], config['api_hash'], app_version='1.0',
+                            device_model='thevdsapbot')
+    return client
