06f3731f650609bb3e0029d5c3534f2a02f79985
vdsap <thevdsap@gmail.com>
2024-08-29 15:48:51 +0300

    Change config for log

--- Logs.py
+++ Logs.py
@@ -1,19 +1,25 @@
+
 class TgLogger:
     bot = None
     LogsQueue = []
-    RangeLoggPush = 10
-    Chat_ID = -1002222067956
+    CountLogsSend = 0
+    conf = None
+    Chat_ID = 0
 
-    def __init__(self, bot):
+    def __init__(self, bot, conf):
         TgLogger.bot = bot
+        TgLogger.conf = conf
+        TgLogger.Chat_ID = TgLogger.conf["logs_chatid"]
+        TgLogger.CountLogsSend = TgLogger.conf["count_logs_send"]
 
     async def receiving_method(self, record):
         text = record
         text = f"""{text.split(".")[0]}\n"""
         TgLogger.LogsQueue.append(text)
 
-        if len(TgLogger.LogsQueue) >= TgLogger.RangeLoggPush:
+        if len(TgLogger.LogsQueue) >= TgLogger.CountLogsSend:
+            print(TgLogger.LogsQueue)
             text = ""
-            for i in range(TgLogger.RangeLoggPush):
+            for i in range(TgLogger.CountLogsSend):
                 text += TgLogger.LogsQueue.pop(-1)
             await self.bot.send_message(TgLogger.Chat_ID, f"<pre>{text}</pre>")
